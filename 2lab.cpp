12054adbb6eeab10e954dde69b2698638ad89114
Henaa810 <henaaaaaaahhh@gmail.com>
2026-09-16 07:38:49 +0530

    Add two-pointer solution to find indices of target sum

---
+++ 2lab.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nums[] = {2, 7, 11, 15};
+    int n = 4;
+    int target = 9;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                cout << "[" << i << ", " << j << "]";
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
